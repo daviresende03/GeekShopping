a95199b6b4ee3bda55bdbde3fe269db93e1aed79
daviresende03 <daviresendedev@gmail.com>
2023-01-29 20:37:47 -0300

    Subindo primeiro migration - tabela product

--- GeekShopping.ProductApi/Model/Context/MySqlContext.cs
+++ GeekShopping.ProductApi/Model/Context/MySqlContext.cs
@@ -4,7 +4,10 @@ namespace GeekShopping.ProductApi.Model.Context
 {
     public class MySqlContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
         public MySqlContext() {}
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) {} 
+
     }
 }
